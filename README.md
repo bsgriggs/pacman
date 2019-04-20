9d6323cbb2f05ad22c6a104758e39679e36bf46f
bsgriggs <bsgriggs@purdue.edu>
2019-04-19 20:51:36 -0700

    reference conversion

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pacman
+# tetris
 
 CNIT 315 Class Project
 
@@ -7,3 +7,40 @@ Nicole Griffin - griff125@purdue.edu
 Zayyad Atekoja - zatekoja@purdue.edu
 Allison Babilonia - ababilo@purdue.edu
 David Szymanski - szymansk@purdue.edu
+
+ToDo:
+>add a main menu with a title, controls, and options 0) exit 1) play game 2) view high scores
+>Use a queue of blocks to show upcoming blocks, pull from the queue when adding to the game
+>always print the queue on top of the game area, next block to the left
+>when the game starts, read scores from a file and put them in a linked list
+>When the game is over, ask for a name and add them to the list with their score. Then, ask if they want to play again. If they dont want to play again, save the linked list to the file.
+
+Extra:
+>Add block reserve
+>Add option to change controls, game area size, and or starting level
+
+Sample Game Area ():
+{Current Score: 0 | Current Level: 1}
+======================
+|                    |
+|                    |
+|                    |
+|                    |
+|        # #         |
+|        # #         |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|@               @   |
+|@               @   |
+|@   X Z Z   # # @   |
+|@ X X X Z Z # # @   |
+======================
+
--- tetris.c
+++ tetris.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
-#include <signal.h>
 #include <time.h>
 #include <fcntl.h>
 
 typedef struct level{
+	int l;
 	int score;
 	int nsec;
 }level;
+
+typedef struct blockNode{
+	char data[5][5];
+	int width;
+	int height;
+}blockNode;
+
+typedef struct playerNode{
+	char name[20];
+	int score;
+	struct tm date;
+}playerNode;
 
 typedef struct game{
 	//2d array of the game UI
@@ -18,12 +30,14 @@ typedef struct game{
 	int level;
 	int gameover;
 	int score;
-	//struct for new blocks
+	//struct of current falling block
 	struct block{
 		char data[5][5];
 		int width;
 		int height;
 	}block;
+	//pointer to a queue of next blocks
+	blockNode * queue;
 	int x;
 	int y;
 }game;
@@ -47,36 +61,112 @@ struct block blocks[] ={
 	  " &"}, 2, 3},
 
 	{{"ZZ ",
-	  " ZZ"},3, 2}
+	  " ZZ"},3, 2},
+
+	{{" $$",
+	  "$$ "},3, 2}
 };
 
 //array of levels with required scores, timespec tv_nsec values
 //where nsec is nanoseconds of elapsed time
 struct level levels[]={
-	{0, 1200000},
-	{1500, 900000},
-	{8000, 700000},
-	{20000, 500000},
-	{40000, 400000},
-	{75000, 300000},
-	{100000, 200000}
+	{1, 0, 1300000},
+	{2, 1000, 1100000},
+	{3, 5000, 900000},
+	{4, 10000, 700000},
+	{5, 20000, 500000},
+	{6, 30000, 400000},
+	{7, 40000, 300000},
+	{8, 50000, 200000},
+	{9, 60000, 100000},
+	{10, 70000, 80000}
 };
 
 #define BLOCKS_SIZE (sizeof(blocks)/sizeof(struct block))
 #define LEVELS_SIZE (sizeof(levels)/sizeof(struct level))
+#define GAME_AREA_WIDTH 10
+#define GAME_AREA_HEIGHT 20
 
+//terminal IO initiate state
 struct termios save;
 
 //global prototype definitions
-void initialize(int, int, game*);
-void printGame(game*);
-int collisionTest(game*);
+void runTetris();
+void initialize(int, int, game *);
+void printGame(game *);
+int collisionTest(game *);
+void pickBlock(game *);
+void blockGravity(game *);
+void blockFastFall(game *, struct timespec*);
+void printBlock(game *);
+void checkCompleteLine(game *);
+void fallLine(game *, int);
+int checkLevelFromScore(game *);
+void rotateBlock(game *);
+
+void returnTerminal();
+void setTerminal();
+void cleanMemory(game *);
 
 int main(){
-	
-
-	return 1;
+	runTetris();
 }// main end
+
+void runTetris(){
+	struct timespec tm;
+	game g;
+	char input;
+	int count=0;
+	setTerminal();
+	initialize(GAME_AREA_WIDTH, GAME_AREA_HEIGHT, &g);
+	srand(time(NULL));
+
+	tm.tv_sec=0;
+	tm.tv_nsec=1000000;
+
+	pickBlock(&g);
+	while (!g.gameover) {
+		nanosleep(&tm, NULL);
+		count++;
+		if (count%50 == 0) {
+			printGame(&g);
+		}
+		if (count%350 == 0) {
+			blockGravity(&g);
+			checkCompleteLine(&g);
+		}
+		while ((input=getchar())>0) {
+			switch (input) {
+				case 'a':
+				g.x--;
+				if (collisionTest(&g))
+				g.x++;
+				break;
+				case 'd':
+				g.x++;
+				if (collisionTest(&g))
+				g.x--;
+				break;
+				case 's':
+				blockGravity(&g);
+				break;
+				case ' ':
+				rotateBlock(&g);
+				break;
+				case 'w':
+				blockFastFall(&g, &tm);
+				break;
+			}
+		}
+		tm.tv_nsec=checkLevelFromScore(&g);
+	}
+
+	printGame(&g);
+	printf("*** GAME OVER ***\n");
+
+	cleanMemory(&g);
+	returnTerminal();
+} //runTetris end
 
 //set defaults and allocated memory for the game UI
 void initialize(int width, int height, game *g) {
@@ -90,9 +180,9 @@ void initialize(int width, int height, game *g) {
 	
 	//allocated memory for the game UI
 	g->game = malloc(sizeof(char *)*width);
-	for (x=0; x<width; x++) {
+	for (x = 0; x < width; x++) {
 		g->game[x] = malloc(sizeof(char)*height);
-		for (y=0; y<height; y++)
+		for (y = 0; y < height; y++)
 			g->game[x][y] = ' ';
 	}
 }//initialize end
@@ -101,25 +191,24 @@ void initialize(int width, int height, game *g) {
 void printGame(game *g) {
 	int x,y;
 	//seperate new UI from last
-	for (x=0; x<30; x++)
+	for (x = 0; x < 30; x++)
 		printf("\n");
 	//display score info
-	printf("{Current Score: %d | Current Level: %d]\n", g->score, g->level);
+	printf("{Score: %d | Level: %d}\n", g->score, g->level);
 	//print top border
-	for (x=0; x<2*g->width+2; x++)
+	for (x = 0; x < 2 * g->width + 2; x++)
 		printf("=");
 	printf("\n");
 	//move down UI
-	for (y=0; y<g->height; y++) {
+	for (y = 0; y < g->height; y++) {
 		// print side border
 		printf ("|");
-		for (x=0; x<g->width; x++) {
-			// print block if it is should be in that position
-			if (x>=g->x && y>=g->y
-			 && x<(g->x+g->block.width)
-			 && y<(g->y+g->block.height) 
-			 && g->block.data[y-g->y][x-g->x]!=' ')
-				printf("%c ", g->block.data[y-g->y][x-g->x]);
+		for (x = 0; x < g->width; x++) {
+			// print block if it should be in that position
+			if (x >= g->x && y >= g->y && x < (g->x + g->block.width)
+			 && y < (g->y + g->block.height) && g->block.data[y - g->y][x - g->x] != ' '){
+				printf("%c ", g->block.data[y - g->y][x - g->x]);
+			}
 			// else print the game UI array value for that postion
 			else
 				printf("%c ", g->game[x][y]);
@@ -129,7 +218,7 @@ void printGame(game *g) {
 	} // move down UI end
 
 	//print bottom border
-	for (x=0; x<2*g->width+2; x++)
+	for (x = 0; x < 2 * g->width + 2; x++)
 		printf("=");
 	//get to next line for input
 	printf("\n");
@@ -138,22 +227,23 @@ void printGame(game *g) {
 //return 1 if the piece cant move in a direction
 int collisionTest(game *g) {
 	int testX, testY;
-	struct block current = g->block;
-
-	for (int x = 0; x < current.width; x++)
-		for (int y = 0; y < current.height; y++) {
+	for (int x = 0; x < g->block.width; x++)
+		for (int y = 0; y < g->block.height; y++) {
 			testX = g->x + x;
 			testY = g->y + y;
+			//if attempted move is outside the borders
 			if ( testX < 0 || testX >= g->width)
 				return 1;
-			if ( current.data[y][x]!=' ') {
-				if (( testY >= g->height) || (testX >= 0 && testX < g->width 
-					&& testY >= 0 && g->game[testX][testY]!=' '))
+			//if the attempted move has a char there
+			if ( g->block.data[y][x]!=' ') {
+				if (( testY >= g->height) || (testX >= 0 && testX < g->width && testY >= 0 &&
+					g->game[testX][testY]!=' ')){
 					return 1;
+				}
 		}
 	}
 	return 0;
-}
+} //collisionTest end
 
 //load the game with a new random block
 void pickBlock(game *g) {
@@ -163,8 +253,147 @@ void pickBlock(game *g) {
 	g->x = (g->width / 2) - (g->block.width / 2);
 	//align block to top
 	g->y = 0;
-	//if the block cant go down while its at the top, then the game is over
+	//if the block cant go down while it is at the top, then the game is over
 	if (collisionTest(g)) {
 		g->gameover=1;
 	}
+} //pickBlock end
+
+//moves the block down
+void blockGravity(game *g) {
+	int x,y;
+	//move block down
+	g->y++;
+	//check if block hit bottom
+	if (collisionTest(g)) {
+		g->y--;
+		printBlock(g);
+		pickBlock(g);
+	}
+} //blockGravity end
+
+//moves the block all the way down
+void blockFastFall(game *g, struct timespec* t) {
+	while (collisionTest(g) == 0){
+		t->tv_nsec = 1;
+		g->y++;
+	}
+	t->tv_nsec = levels[g->level].nsec;
+	g->y--;
+	printBlock(g);
+	pickBlock(g);
+} //blockGravity end
+
+//updates the game string to have the block in it
+void printBlock(game *g) {
+	int X,Y;
+	for (int x = 0; x < g->block.width; x++)
+		for (int y = 0; y < g->block.height; y++) {
+			if (g->block.data[y][x] != ' ')
+				g->game[g->x + x][g->y + y] = g->block.data[y][x];
+	}
+} //printBlock end
+
+//look for full lines and update score
+void checkCompleteLine(game *g) {
+	int isComplete;
+	int p = 50;
+	//game string loop
+	for (int y = g->height - 1; y >= 0; y--) {
+		isComplete = 1;
+		//line loop
+		for (int x = 0; x < g->width && isComplete; x++) {
+			//if any point on the line isnt complete
+			if (g->game[x][y] == ' ') {
+				isComplete = 0;
+			}
+		}
+		// if the line is complete, update score, move blocks down
+		if (isComplete) {
+			g->score += p + 50 * levels[g->level].l;
+			//give more points for many lines cleared at once
+			p *= 2;
+			fallLine(g, y);
+			y++;
+		}
+	}
+} // checkClearedLines end
+
+//move everything above the cleared line down 1
+void fallLine(game *g, int line) {
+	int x;
+	//start at the complete line and move up
+	for (int y = line; y > 0; y--) {
+		for (x = 0; x < g->width; x++)
+			g->game[x][y] = g->game[x][y-1];
+	}
+	//?
+	//for (x = 0; x < g->width; x++)
+		//g->game[x][0] = ' ';
+} //fall end
+
+// returns the nsec of the level the user is on, updates the level if the user leveled up
+int checkLevelFromScore(game *g) {
+	for (int i = 0; i < LEVELS_SIZE; i++) {
+		//the players score is enough to increase the level
+		if (g->score >= levels[i].score) {
+			g->level = i+1;
+		} else break;
+	}
+	return levels[g->level-1].nsec;
+} // checkLevelFromScore end
+
+//rotates the games current block and checks for collision
+void rotateBlock(game *g) {
+	//block to be rotated
+	struct block rotate = g->block;
+	//store the original state 
+	struct block safe = rotate;
+	int x,y;
+	rotate.width = safe.height;
+	rotate.height = safe.width;
+	//rotate the block
+	for (x = 0; x < safe.width; x++)
+		for (y = 0; y < safe.height; y++) {
+			rotate.data[x][y] = safe.data[safe.height - y - 1][x];
+		}
+	//save the original position in case of restore
+	x = g->x;
+	y = g->y;
+	//align the rotated block to the same position as the original block
+	g->x -= (rotate.width - safe.width) / 2;
+	g->y -= (rotate.height - safe.height) / 2;
+	//set the game's block to the rotated one
+	g->block = rotate;
+	//if the rotated block colides with something, restore the safe version
+	if (collisionTest(g)) {
+		g->block = safe;
+		g->x = x;
+		g->y = y;
+	}
+} // rotateBlock end
+
+//return terminal to pre-game state
+void returnTerminal() {
+	tcsetattr(fileno(stdin),TCSANOW,&save);
 }
+
+//Make input auto-enter?
+void setTerminal() {
+	struct termios custom;
+	int fd = fileno(stdin);
+	tcgetattr(fd, &save);
+	custom=save;
+	custom.c_lflag &= ~(ICANON|ECHO);
+	tcsetattr(fd,TCSANOW,&custom);
+	fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0)|O_NONBLOCK);
+}
+
+//release memory
+void cleanMemory(game *g) {
+	int x;
+	for (x = 0; x < g->width; x++) {
+		free(g->game[x]);
+	}
+	free(g->game);
+}// cleanMemory end
